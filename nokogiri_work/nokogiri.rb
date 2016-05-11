a28debfdcb10eae4393c3454ec62ffdf8395af6f
Brian Bancroft <bancroft.bw@gmail.com>
2016-05-11 13:34:53 -0400

    some nokogiri

--- nokogiri_work/nokogiri.rb
+++ nokogiri_work/nokogiri.rb
@@ -3,17 +3,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-
-# page = "http://www.parl.gc.ca/Parliamentarians/en/constituencies/"
-#
-#
-# local_directory = "constit.html"
-# local_mp = "single_constit.html"
-#
-# #For production, this will require a different approach
-# doc = Nokogiri::HTML(open(local_mp))
-
-
 def getConstitInfo(page)
 
   doc = Nokogiri::HTML(open(htmlDOC))
@@ -32,10 +21,27 @@ def getConstitInfo(page)
     index += 1
     mpList.push({:name => person.text, :mp_id =>
      regexp.match(person.attributes["href"])[1]})
-
   end
+end
 
-  end
+def getMPInfo(page)
+doc = Nokogiri::HTML(open(mpdoc))
+pictureURL = doc.css('.profile img')[0]
+
+end
+
+
+# page = "http://www.parl.gc.ca/Parliamentarians/en/constituencies/"
+#
+#
+# local_directory = "constit.html"
+# local_mp = "single_constit.html"
+#
+# #For production, this will require a different approach
+# doc = Nokogiri::HTML(open(local_mp))
+
+
+ mpdoc = "sample_mp.html"
 
 
   htmlDOC = "constit.html"
