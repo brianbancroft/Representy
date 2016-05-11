48c1f869885d0ef0f072607ecdf6c793fbd39511
kristincheverie <kcheverie@gmail.com>
2016-05-11 15:29:41 +0000

    add skeleton models

--- politican-finder/app/controllers/application_controller.rb
+++ politican-finder/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @mps = Mp.all
+  end
+
+  def show
+    @mp = Mp.find(params[:id])
+  end
+
 end
