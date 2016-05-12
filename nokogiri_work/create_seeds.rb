c799e370fc8b957e3bee70b1b9c07803ce746dfa
Brian Bancroft <bancroft.bw@gmail.com>
2016-05-12 13:46:11 -0400

    new scraping

---
+++ nokogiri_work/create_seeds.rb
@@ -0,0 +1,27 @@
+require 'pry'
+require 'json'
+
+#Creates a textfile for seeding the database that can be copied and pasted for
+# seeds.rb. each line should have the following format:
+#   Country.create(name: 'Germany', population: 81831000)
+
+
+def write_mp_seed(name, party, address, phone, email, photourl, ridingID)
+  string_ouput = "Mp.seed(name:#{name}, party: #{party}, cons_address: #{address}, cons_phone: #{phone}, email: #{email}, photo: #{photourl}, riding_id: #{ridingID})"
+end
+
+
+def write_riding_seed()
+
+
+end
+
+
+file = File.read('file-name-to-be-read.json')
+mp_hash = JSON.parse(file)
+
+open('mp_seeds.txt', 'w') { |f|
+  mp_hash.each do |mp|
+    f << write_mp_seed(mp[:name], mp[:party], mp[:address],mp[:phone],mp[:email], mp[:photo], mp[:riding_id])
+  end
+}
