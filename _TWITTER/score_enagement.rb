48e00a908a1b2ab8fb6cdead849e9877c5902ea0
Brian Bancroft <bancroft.bw@gmail.com>
2016-05-20 11:59:44 -0400

    basic normalization formula

--- _TWITTER/score_enagement.rb
+++ _TWITTER/score_enagement.rb
@@ -1,12 +1,49 @@
+require 'math'
 class Score_engagmenet
 
-  def self.test(tweet)
-    if tweet.text == '@' || tweet.text == '.' && tweet.text == '@'
-      partcipation_count += 1
-    elsif not tweet.quoted_status.text.nil?
-      partcipation_count += 1
+  def self.test(tweets)
+    participation_count = 0
+    tweets.each do |tweet|
+      if tweet.text == '@' || tweet.text == '.' && tweet.text == '@'
+        participation_count += 1
+      elsif not tweet.quoted_status.text.nil?
+        participation_count += 1
+      end
     end
-    return partcipation_count
+    participation_count = (participation_count / tweets.length) * 100
+    return participation_count
+  end
+
+  #Supporter Math Functions - taken from http://stackoverflow.com/questions/7749568/how-can-i-do-standard-deviation-in-ruby.
+  def self.sum
+    self.inject(0){|accum, i| accum + i }
+  end
+
+  def self.mean
+    self.sum/self.length.to_f
+  end
+
+  def self.sample_variance
+    m = self.mean
+    sum = self.inject(0){|accum, i| accum +(i-m)**2 }
+    sum/(self.length - 1).to_f
+  end
+
+  def self.standard_deviation
+    return Math.sqrt(self.sample_variance)
+  end
+
+  # Takes in a list of engagement scores ()
+  def self.normalize_scores(engagement_scores)
+    normalized_scores = []
+    #Calculate Mean and Standard Deviation
+    mean = 0
+    stddev = 0
+
+    engagement_scores.each do |score|
+      normalized_scores.push(Math.abs(score-mean)/stddev)
+    end
+    return normalized_scores
   end
 
 end
