f93a30ec99b46edd6a33ab5dc63ac500c8801a98
achandi <chandi.ajit@gmail.com>
2016-05-21 18:22:46 -0400

    twitter progress before pulling brians data - dont merge pleaase

--- _TWITTER/extract_tweets.rb
+++ _TWITTER/extract_tweets.rb
@@ -11,14 +11,14 @@ require 'pry'
 
 
 @client = Twitter::REST::Client.new do |config|
-  # config.consumer_key        = "b1HRb0wLGXLT1ZJNSHuhJA5ce"
-  # config.consumer_secret     = "2bZAqKlyGhjW6FU3EOLmFIMpo1ajpnrgy0XGv4YhqnNwv2z2Bh"
-  # config.access_token        = "444219990-U2IYBdYC4W6N4tQuNgl49eXWrLXIWBAioXB7hwOC"
-  # config.access_token_secret = "U2RpWh0zYboBJQS1lI0oy6KlOFvOA8e2n5JHuJEs34VID"
-  config.consumer_key        = "E6ut0P8Bdig8RsvWByomuUiSQ"
-  config.consumer_secret     = "vvt9JY7B3tyf8PwE2vChADKW0sT553hiL9Z5gHTqbClp13MOd8"
-  config.access_token        = "1571155201-DGxRsqhbE43Y0NSTSgJv4ljL3mEorIMB12xDHit"
-  config.access_token_secret = "lRFb792fOQB8ERT9ND0xLruqL0nKPKsFGXoaXQ3l9inDJ"
+  config.consumer_key        = "b1HRb0wLGXLT1ZJNSHuhJA5ce"
+  config.consumer_secret     = "2bZAqKlyGhjW6FU3EOLmFIMpo1ajpnrgy0XGv4YhqnNwv2z2Bh"
+  config.access_token        = "444219990-U2IYBdYC4W6N4tQuNgl49eXWrLXIWBAioXB7hwOC"
+  config.access_token_secret = "U2RpWh0zYboBJQS1lI0oy6KlOFvOA8e2n5JHuJEs34VID"
+  # config.consumer_key        = "E6ut0P8Bdig8RsvWByomuUiSQ"
+  # config.consumer_secret     = "vvt9JY7B3tyf8PwE2vChADKW0sT553hiL9Z5gHTqbClp13MOd8"
+  # config.access_token        = "1571155201-DGxRsqhbE43Y0NSTSgJv4ljL3mEorIMB12xDHit"
+  # config.access_token_secret = "lRFb792fOQB8ERT9ND0xLruqL0nKPKsFGXoaXQ3l9inDJ"
 end
 
 json_file = File.read('twitter.json')
@@ -47,8 +47,9 @@ json_data.each do |mp|
       })
     end
   # rescue Twitter::Error::TooManyRequests => error
-  rescue Twitter::Error::NotFound => error
+  rescue Twitter::Error::Unauthorized => error
     binding.pry
+    # sleep error.rate_limit.reset_in + 1
   end
 end
 
---
+++ _TWITTER/extract_tweets_laith_before_error.rb
@@ -0,0 +1,46 @@
+require 'twitter'
+require 'pry'
+require 'json'
+require 'rubygems'
+require 'open-uri'
+require 'rest-client'
+require 'json'
+require 'pry'
+
+@client = Twitter::REST::Client.new do |config|
+  # config.consumer_key        = "b1HRb0wLGXLT1ZJNSHuhJA5ce"
+  # config.consumer_secret     = "2bZAqKlyGhjW6FU3EOLmFIMpo1ajpnrgy0XGv4YhqnNwv2z2Bh"
+  # config.access_token        = "444219990-U2IYBdYC4W6N4tQuNgl49eXWrLXIWBAioXB7hwOC"
+  # config.access_token_secret = "U2RpWh0zYboBJQS1lI0oy6KlOFvOA8e2n5JHuJEs34VID"
+  config.consumer_key        = "E6ut0P8Bdig8RsvWByomuUiSQ"
+  config.consumer_secret     = "vvt9JY7B3tyf8PwE2vChADKW0sT553hiL9Z5gHTqbClp13MOd8"
+  config.access_token        = "1571155201-DGxRsqhbE43Y0NSTSgJv4ljL3mEorIMB12xDHit"
+  config.access_token_secret = "lRFb792fOQB8ERT9ND0xLruqL0nKPKsFGXoaXQ3l9inDJ"
+end
+
+json_file = File.read('twitter.json')
+json_data = JSON.parse(json_file)
+twitter = []
+data = []
+
+# json_data.each do |i| 
+#   twitter.push(i['handle'])
+# end
+
+json_data.each do |mp|
+  begin
+    # puts mp['handle'].gsub('@', '')
+    @client.user_timeline(mp['handle'].gsub('@', ''), {count: 50}).each do |tweet|
+      data.push({
+        "text": tweet.text,
+        "quoted_status": {text: tweet.quoted_status.text}  
+      })
+    end
+  rescue Twitter::Error::TooManyRequests => error
+    sleep error.rate_limit.reset_in + 1
+  end
+end
+
+File.open("tweets.json","w") do |f|
+  f.write(data.to_json)
+end
