e8934102f870aeec9b8e32fdb3e5b6d20ef9841f
Brian Bancroft <bancroft.bw@gmail.com>
2016-05-22 18:15:07 -0400

    backend can query mp_id and riding geometry

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,13 +16,28 @@ app.use(cors());
 app.use(koaPg('postgres://dkuuauoezstjor:Lr6qZtm1TlJHJJoellAJd5_Yni@ec2-54-227-245-222.compute-1.amazonaws.com:5432/d2imlo0f5r5jov'));
 
 param('riding',function*(ridingid,next) {
-  var query = 'SELECT  ST_AsGeoJSON(geom) FROM election_boundaries_joined_simp1 WHERE riding_id=' + ridingid; //analog - seek params[:id] analog with this library
-  var riding = yield this.pg.db.client.query_(query)
-  this.geom = riding.rows[0].st_asgeojson;
-  this.ridingName = "riding name";
-  this.mpName = "mp_name";
-  this.mpID= "mp_id";
-  this.partyName = "party_name";
+
+  var query0 = 'SELECT  (mp_id,ST_AsGeoJSON(geom)) FROM election_boundaries_joined_simp1 WHERE riding_id=' + ridingid; //analog - seek params[:id] analog with this library
+  var geomQuery = yield this.pg.db.client.query_(query0)
+  // var query1 = 'SELECT (mp_id) FROM election_boundaries_joined_simp1 WHERE riding_id=' + ridingid;
+  // var mpIDQuery = yield this.pg.db.client.query_(query1)
+  // var query2 = 'SELECT  (riding_nam) FROM election_boundaries_joined_simp1 WHERE riding_id=' + ridingid;
+  // var ridingNameQuery = yield this.pg.db.client.query_(query2)
+  // var query3 = 'SELECT (party) FROM election_boundaries_joined_simp1 WHERE riding_id=' + ridingid;
+  // var partyNameQuery = yield this.pg.db.client.query_(query3)
+  // var query4 = 'SELECT (name) FROM election_boundaries_joined_simp1 WHERE riding_id=' + ridingid;
+  // var mpNameQuery = yield this.pg.client.query_(query4)
+
+  // this.mpID = mpIDQuery.rows[0].mp_id;
+  // this.ridingName = ridingNameQuery.rows[0].riding_nam;
+  // this.geom = geomQuery.rows[0].st_asgeojson;
+  // this.partyName = partyNameQuery.rows[0].riding_nam;
+  // this.mpName = mpNameQuery.rows[0].name
+
+
+  // this.ridingName = "riding name";
+  // this.mpName = "mp_name";
+  // this.partyName = "party_name";
   yield next;
 });
 
@@ -56,13 +71,17 @@ app.use(route.get('/members', function*() {
 }));
 
 app.use(route.get('/riding/:riding', function*() {
-  this.body = {
-    ridingName: this.ridingName,
-    mpName: this.mpName,
-    mpID: this.mpID,
-    partyName: this.partyName,
-    ridingGeom: this.geom
-  };
+  // this.body = {
+  //   type: "Feature",
+  //   properties: {
+  //     ridingName: this.ridingName,
+  //     mpName: this.mpName,
+  //     mpID: this.mpID,
+  //     partyName: this.partyName,
+  //   },
+  //   geometry: JSON.parse(this.geom)
+  // };
+  this.body = "MP " + this.mpID + ", name: " + this.mpName ;
 }));
 
 app.use(route.post('/location/:long&lat', function*() {
@@ -73,10 +92,6 @@ app.use(route.post('/location/:long&lat', function*() {
 }))
 
 app.use(json());
-
-// SQLLOCATION = 'postgres://dkuuauoezstjor:Lr6qZtm1TlJHJJoellAJd5_Yni@ec2-54-227-245-222.compute-1.amazonaws.com:5432/d2imlo0f5r5jov';
-// app.use(koaPg(SQLLOCATION));
-
 
 app.listen(3000);
 console.log('Koa listening on port 3000');
