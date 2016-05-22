bd82b8d77ceb41450dda598bff257754546163a8
Brian Bancroft <bancroft.bw@gmail.com>
2016-05-22 11:47:03 -0400

    new version of the ridings SQL generator seed for Postgres

--- _GEO/seed_ridings_table.rb
+++ _GEO/seed_ridings_table.rb
@@ -1,22 +1,20 @@
 require 'pry'
 require 'json'
 
-def write_riding_seed(id, name_en, name_fr,mp_id, party, riding_geom)
-  string_ouput = "#{id},\'#{name_en}\',\'#{name_fr}\',#{mp_id},\'#{party}\', ST_GeomFromGeoJSON(\'{\"type\":\"MultiPolygon\",\"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:EPSG::4326\" } }, \"coordinates\":#{riding_geom}}\')"
+def write_riding_seed(id, mp_id,mp_name, party,riding_name, riding_geom)
+  string_ouput = "#{id},#{mp_id},\'#{mp_name}\',\'#{party}\',\'#{riding_name}\', ST_GeomFromGeoJSON(\'{\"type\":\"MultiPolygon\",\"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:EPSG::4326\" } }, \"coordinates\":#{riding_geom}}\')"
 end
 
-geoFile = File.read('ridings_simplified.geojson')
+geoFile = File.read('consolidated_shapefiles/full_elec_boundaries.geojson')
 geo_hash = JSON.parse(geoFile)
 
-scrapeFile = File.read('constituencies_info.json')
-riding_hash = JSON.parse(scrapeFile)
+binding.pry
 
-open('ridings_seed.sql','w') {|sql_file|
+open('ridings_seedv2.sql','w') {|sql_file|
   geo_hash["features"].each do |riding|
-    support_file = riding_hash.select { |element| element["ridingID"].to_i == riding["properties"]["constit_id"] }[0]
-    sql_file << "INSERT INTO ridings (id, name_en, name_fr,mp_id, party, riding_geom)\n"
+    sql_file << "INSERT INTO ridings (id, mp_id, mp_name,riding_name, party, riding_geom)\n"
     sql_file << "VALUES ("
-    sql_file << write_riding_seed(riding["properties"]["constit_id"],riding["properties"]["ENNAME"],riding["properties"]["FRNAME"],support_file["mpID"],support_file["party"],riding["geometry"]["coordinates"])
+    sql_file << write_riding_seed(riding["properties"]["constit_id"],riding["properties"]["MP_ID"],riding["properties"]["Name"],riding["Party"],riding["Riding_Nam"],riding["geometry"]["coordinates"])
     sql_file << ");\n"
   end
 
