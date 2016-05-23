2ae2eac0e07f240a0cfe3c2cb525aaf1a57de782
Kristin Cheverie <kcheverie@gmail.com>
2016-05-23 19:15:32 -0400

    change scroll position on view render

--- frontend/src/components/SingleRidingMap.jsx
+++ frontend/src/components/SingleRidingMap.jsx
@@ -4,41 +4,125 @@ var SingleRidingMap = React.createClass({
 
   componentDidMount: function(){
     var riding = this.props.mp.riding_id
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpYW5iYW5jcm9mdCIsImEiOiJsVGVnMXFzIn0.7ldhVh3Ppsgv4lCYs65UdA';
-     var map = new mapboxgl.Map({
-         container: 'map',
-         style: 'mapbox://styles/brianbancroft/cio5y4bf10001afnmjjdelbzf',
-         zoom: 1,
-         center: [-91.23046,45.460130637]
-     });
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpYW5iYW5jcm9mdCIsImEiOiJsVGVnMXFzIn0.7ldhVh3Ppsgv4lCYs65UdA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/brianbancroft/cio5y4bf10001afnmjjdelbzf',
+        zoom: 2,
+        center: [-91.23046875,45.460130637921]
+    });
 
-     map.on('load', function () {
-         var data =
-         map.addSource('urban-areas', {
-             'type': 'geojson',
-             //'data': {"type":"Polygon","coordinates":[[[-91.23046875,45.460130637921],[-79.8046875,49.837982453085],[-69.08203125,43.452918893555],[-88.2421875,32.694865977875],[-91.23046875,45.460130637921]]]}
-             //compare: {"type":"Polygon","coordinates":[[[-91.23046875,45.460130637921],[-79.8046875,49.837982453085],[-69.08203125,43.452918893555],[-88.2421875,32.694865977875],[-91.23046875,45.460130637921]]]}
-             'data': 'http://localhost:3000/riding/' + riding
-             //'data': 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_urban_areas.geojson'
-         });
+    map.on('style.load', function(){
+      $.getJSON('http://localhost:3000/riding/' + riding, function(response){
+        var boundingBox = getBoundingBox(response);
+        var ridingBoundary = new mapboxgl.GeoJSONSource({ data: response } );
 
-         map.addLayer({
-             'id': 'urban-areas-fill',
-             'type': 'fill',
-             'source': 'urban-areas',
-             'layout': {},
-             'paint': {
-                 'fill-color': '#f08',
-                 'fill-opacity': 0.4
-             }
-         // This is the important part of this example: the addLayer
-         // method takes 2 arguments: the layer as an object, and a string
-         // representing another layer's name. if the other layer
-         // exists in the stylesheet already, the new layer will be positioned
-         // right before that layer in the stack, making it possible to put
-         // 'overlays' anywhere in the layer stack.
-         }, 'water');
-     });
+        map.addSource('riding', ridingBoundary);
+        map.addLayer({
+          'id': 'whig-riding',
+          'type': 'fill',
+          'source': 'riding',
+          'paint': {
+            'fill-color': '#f00',
+            'fill-opacity': 0.4
+          },
+          filter: ['==', 'partyName', 'Liberal']
+        });
+        map.addLayer({
+          'id': 'tory-riding',
+          'type': 'fill',
+          'source': 'riding',
+          'paint': {
+            'fill-color': '#00F',
+            'fill-opacity': 0.4
+          },
+          filter: ['==', 'partyName', 'Conservative']
+        });
+        map.addLayer({
+          'id': 'dipper-riding',
+          'type': 'fill',
+          'source': 'riding',
+          'paint': {
+            'fill-color': '#ffa500',
+            'fill-opacity': 0.4
+          },
+          filter: ['==', 'partyName', 'NDP']
+        });
+        map.addLayer({
+          'id': 'green-riding',
+          'type': 'fill',
+          'source': 'riding',
+          'paint': {
+            'fill-color': '#0F0',
+            'fill-opacity': 0.4
+          },
+          filter: ['==', 'partyName', 'Green Party']
+        });
+        map.addLayer({
+          'id': 'bloc-riding',
+          'type': 'fill',
+          'source': 'riding',
+          'paint': {
+            'fill-color': '#88F',
+            'fill-opacity': 0.4
+          },
+          filter: ['==', 'partyName', 'Bloc Quebecois']
+        });
+
+        map.fitBounds([[boundingBox.xMin, boundingBox.yMin], [boundingBox.xMax, boundingBox.yMax]]);
+      })
+
+
+
+    });
+
+    function getBoundingBox(data) {
+      var bounds = {}, coords, point, latitude, longitude;
+      for (var i = 0; i < data.geometry.coordinates.length; i++) {
+        var polygons = data.geometry.coordinates[i];
+
+        for (var j = 0; j < polygons.length; j++) {
+          coords = polygons[j];
+
+          for (var k = 0; k < coords.length; k++) {
+            longitude = coords[k][0];
+            latitude = coords[k][1];
+            bounds.xMin = bounds.xMin < longitude ? bounds.xMin : longitude;
+            bounds.xMax = bounds.xMax > longitude ? bounds.xMax : longitude;
+            bounds.yMin = bounds.yMin < latitude ? bounds.yMin : latitude;
+            bounds.yMax = bounds.yMax > latitude ? bounds.yMax : latitude;
+          }
+        }
+
+
+      }
+
+      return bounds;
+    }
+
+
+    // map.on('load', function () {
+    //
+    //     var data =
+    //     map.addSource('ridings', {
+    //         'type': 'geojson',
+    //         'data': 'http://localhost:3000/riding/757'
+    //     });
+    //
+    //     map.addLayer({
+    //         'id': 'riding-areas',
+    //         'type': 'fill',
+    //         'source': 'ridings',
+    //         'layout': {},
+    //         'paint': {
+    //             'fill-color': '#f08',
+    //             'fill-opacity': 0.4
+    //         }
+    //     }, 'water');
+    //
+    // });
+    map.scrollZoom.disable();
+    map.dragPan.disable();
 
    
   },
